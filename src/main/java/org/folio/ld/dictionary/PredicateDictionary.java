43ac604e002e114a96d5c12074b6a8050920826e
Pavel Bobylev <131748689+PBobylev@users.noreply.github.com>
2023-10-23 16:25:31 +0600

    MODLD-131: initial set of types and predicates (#1)
    * MODLD-131: initial set of types and predicates
    
    * MODLD-131: properties added, lombok removed
    
    * MODLD-131: interfaces extracted
    
    * MODLD-131: pom.xml - EIS repo, distribution, scm
    
    * MODLD-131: renamed to lib-linked-data-dictionary

---
+++ src/main/java/org/folio/ld/dictionary/PredicateDictionary.java
@@ -0,0 +1,36 @@
+package org.folio.ld.dictionary;
+
+import lombok.Getter;
+import org.folio.ld.dictionary.api.Predicate;
+
+@Getter
+public enum PredicateDictionary implements Predicate {
+  ACCESS_LOCATION("http://bibfra.me/vocab/marc/accessLocation", -7597340833248531885L),
+  CARRIER("http://bibfra.me/vocab/marc/carrier", -8262298498902758692L),
+  CLASSIFICATION("http://bibfra.me/vocab/lite/classification", 554014919457463261L),
+  CONTENT("http://bibfra.me/vocab/marc/content", -8400837019789777596L),
+  CONTRIBUTOR("http://bibfra.me/vocab/lite/contributor", 6734473498479079135L),
+  COPYRIGHT("http://bibfra.me/vocab/marc/copyright", -5763504959160201664L),
+  CREATOR("http://bibfra.me/vocab/lite/creator", -6427036973279413520L),
+  INSTANTIATES("http://bibfra.me/vocab/lite/instantiates", -5307605433297919406L),
+  MAP("http://library.link/vocab/map", 3538761499444069943L),
+  MEDIA("http://bibfra.me/vocab/marc/media", -1307538224118273019L),
+  NULL(null, null),
+  PE_DISTRIBUTION("http://bibfra.me/vocab/marc/distribution", -448045113972492966L),
+  PE_MANUFACTURE("http://bibfra.me/vocab/marc/manufacture", -2828753452541607170L),
+  PE_PRODUCTION("http://bibfra.me/vocab/marc/production", 2801478436215503730L),
+  PE_PUBLICATION("http://bibfra.me/vocab/marc/publication", 9018504469354013109L),
+  PLACE("http://bibfra.me/vocab/lite/place", -2442655877067158195L),
+  PROVIDER_PLACE("http://bibfra.me/vocab/lite/providerPlace", -5991192843515972558L),
+  STATUS("http://bibfra.me/vocab/marc/status", -6334780726339225629L),
+  TITLE("http://bibfra.me/vocab/marc/title", 5549969427228480763L);
+
+  private final String uri;
+  private final Long hash;
+
+  PredicateDictionary(String uri, Long hash) {
+    this.uri = uri;
+    this.hash = hash;
+  }
+
+}
---
+++ src/main/java/org/folio/ld/dictionary/Property.java
@@ -0,0 +1,41 @@
+package org.folio.ld.dictionary;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class Property {
+
+  public static final String ASSIGNING_SOURCE = "http://bibfra.me/vocab/marc/localIdAssigningSource";
+  public static final String CODE = "http://bibfra.me/vocab/marc/code";
+  public static final String DATE = "http://bibfra.me/vocab/lite/date";
+  public static final String DIMENSIONS = "http://bibfra.me/vocab/marc/dimensions";
+  public static final String EAN_VALUE = "http://bibfra.me/vocab/marc/ean";
+  public static final String EDITION_STATEMENT = "http://bibfra.me/vocab/marc/edition";
+  public static final String EXTENT = "http://bibfra.me/vocab/lite/extent";
+  public static final String ISSUANCE = "http://bibfra.me/vocab/marc/issuance";
+  public static final String LABEL = "http://bibfra.me/vocab/lite/label";
+  public static final String LABEL_RDF = "http://www.w3.org/2000/01/rdf-schema#label";
+  public static final String LANGUAGE = "http://bibfra.me/vocab/lite/language";
+  public static final String LCNAF_ID = "http://bibfra.me/vocab/marc/lcnafId";
+  public static final String LINK = "http://bibfra.me/vocab/lite/link";
+  public static final String LOCAL_ID_VALUE = "http://bibfra.me/vocab/marc/localId";
+  public static final String MAIN_TITLE = "http://bibfra.me/vocab/marc/mainTitle";
+  public static final String NAME = "http://bibfra.me/vocab/lite/name";
+  public static final String NON_SORT_NUM = "http://bibfra.me/vocab/bflc/nonSortNum";
+  public static final String NOTE = "http://bibfra.me/vocab/lite/note";
+  public static final String PART_NAME = "http://bibfra.me/vocab/marc/partName";
+  public static final String PART_NUMBER = "http://bibfra.me/vocab/marc/partNumber";
+  public static final String PROJECTED_PROVISION_DATE = "http://bibfra.me/vocab/bflc/projectedProvisionDate";
+  public static final String PROVIDER_DATE = "http://bibfra.me/vocab/lite/providerDate";
+  public static final String QUALIFIER = "http://bibfra.me/vocab/marc/qualifier";
+  public static final String RESPONSIBILITY_STATEMENT = "http://bibfra.me/vocab/marc/statementOfResponsibility";
+  public static final String SIMPLE_PLACE = "http://bibfra.me/vocab/lite/place";
+  public static final String SOURCE = "http://bibfra.me/vocab/marc/source";
+  public static final String SUBTITLE = "http://bibfra.me/vocab/marc/subTitle";
+  public static final String SUMMARY = "http://bibfra.me/vocab/marc/summary";
+  public static final String TABLE_OF_CONTENTS = "http://bibfra.me/vocab/marc/tableOfContents";
+  public static final String TARGET_AUDIENCE = "http://bibfra.me/vocab/marc/targetAudience";
+  public static final String TERM = "http://bibfra.me/vocab/marc/term";
+  public static final String VARIANT_TYPE = "http://bibfra.me/vocab/marc/variantType";
+
+}
