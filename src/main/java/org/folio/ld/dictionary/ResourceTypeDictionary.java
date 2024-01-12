055ac8e9b736e51ce2026e6eb8ea70413afa31c1
askhat-abishev <Askhat_Abishev@epam.com>
2024-01-12 17:06:07 +0600

    MODLD-207: Add predicates, types, and properties related to marc field 650

--- src/main/java/org/folio/ld/dictionary/ResourceTypeDictionary.java
+++ src/main/java/org/folio/ld/dictionary/ResourceTypeDictionary.java
@@ -8,8 +8,10 @@ public enum ResourceTypeDictionary implements ResourceType {
   ANNOTATION("http://bibfra.me/vocab/lite/Annotation", -2716756821684516448L),
   CARRIER("http://bibfra.me/vocab/marc/Carrier", 421212736624329420L),
   CATEGORY("http://bibfra.me/vocab/lite/Category", -7237503330348194663L),
+  CONCEPT("http://bibfra.me/vocab/lite/Concept", 6012322771220013009L),
   COPYRIGHT_EVENT("http://bibfra.me/vocab/lite/CopyrightEvent", 7138672891049112604L),
   FAMILY("http://bibfra.me/vocab/lite/Family", -1114722094452060721L),
+  FORM("http://bibfra.me/vocab/lite/Form", 6102579518025542780L),
   IDENTIFIER("http://bibfra.me/vocab/lite/Identifier", 8535666185887263660L),
   ID_EAN("http://bibfra.me/vocab/identifier/Ean", 219046076164824264L),
   ID_ISBN("http://library.link/identifier/ISBN", 3962988838506018431L),
@@ -27,7 +29,9 @@ public enum ResourceTypeDictionary implements ResourceType {
   PROVIDER_EVENT("http://bibfra.me/vocab/lite/ProviderEvent", -1414513180123539856L),
   STATUS("http://bibfra.me/vocab/marc/Status", -5546355039147771611L),
   SUPPLEMENTARY_CONTENT("http://bibfra.me/vocab/marc/SupplementaryContent", 3849426639441431097L),
+  TEMPORAL("http://bibfra.me/vocab/lite/Temporal", 4805333230697475193L),
   TITLE("http://bibfra.me/vocab/marc/Title", 1422402142581052159L),
+  TOPIC("http://bibfra.me/vocab/lite/Topic", -3096999901507785420L),
   VARIANT_TITLE("http://bibfra.me/vocab/marc/VariantTitle", 4846800426734929825L),
   WORK("http://bibfra.me/vocab/lite/Work", -3114907246856331309L);
 
