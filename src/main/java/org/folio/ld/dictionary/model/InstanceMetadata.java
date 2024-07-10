faef93ca6c4cb331a78b7fb2547e7d6194e09fe5
Andrei Bordak <Andrei_Bordak@epam.com>
2024-07-10 10:37:40 +0400

    MODLD-429: AllArgsConstructor for InstanceMetadata

--- src/main/java/org/folio/ld/dictionary/model/InstanceMetadata.java
+++ src/main/java/org/folio/ld/dictionary/model/InstanceMetadata.java
@@ -1,11 +1,13 @@
 package org.folio.ld.dictionary.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Accessors(chain = true)
 public class InstanceMetadata {
   private String inventoryId;
