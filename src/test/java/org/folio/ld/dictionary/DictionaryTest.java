143c0e202c2744227fc228f8f926846a869c74f4
Andrei Bordak <Andrei_Bordak@epam.com>
2024-11-01 14:14:33 +0400

    MODLD-572: Remove public modifier

--- src/test/java/org/folio/ld/dictionary/DictionaryTest.java
+++ src/test/java/org/folio/ld/dictionary/DictionaryTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class DictionaryTest {
+class DictionaryTest {
 
   private static Stream<Arguments> dictionaries() {
     return Stream.of(
