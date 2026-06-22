1333e4a5dc215cdf68b4d9afdf139d49007f3742
PBobylev <pavel_bobylev@epam.com>
2026-06-22 15:05:55 +0500

    MODLD-1082: ResourceKind introduced

---
+++ src/test/java/org/folio/ld/dictionary/specific/ResourceKindTest.java
@@ -0,0 +1,67 @@
+package org.folio.ld.dictionary.specific;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.folio.ld.dictionary.ResourceTypeDictionary;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+class ResourceKindTest {
+
+  @ParameterizedTest
+  @EnumSource(value = ResourceTypeDictionary.class,
+    mode = EnumSource.Mode.INCLUDE,
+    names = {"INSTANCE", "WORK"})
+  void containsBibliographicTypes(ResourceTypeDictionary type) {
+    //when
+    var expectedInstanceTypes = ResourceKind.BIBLIOGRAPHIC;
+
+    //then
+    assertThat(expectedInstanceTypes)
+      .isNotEmpty()
+      .contains(type);
+  }
+
+  @ParameterizedTest
+  @EnumSource(value = ResourceTypeDictionary.class,
+    mode = EnumSource.Mode.EXCLUDE,
+    names = {"INSTANCE", "WORK"})
+  void notContainsBibliographicTypes(ResourceTypeDictionary type) {
+    //when
+    var expectedInstanceTypes = ResourceKind.BIBLIOGRAPHIC;
+
+    //then
+    assertThat(expectedInstanceTypes)
+      .isNotEmpty()
+      .doesNotContain(type);
+  }
+
+  @ParameterizedTest
+  @EnumSource(value = ResourceTypeDictionary.class,
+    mode = EnumSource.Mode.INCLUDE,
+    names = {"PERSON", "FAMILY"})
+  void containsAuthorityTypes(ResourceTypeDictionary type) {
+    //when
+    var expectedAuthorityTypes = ResourceKind.AUTHORITY;
+
+    //then
+    assertThat(expectedAuthorityTypes)
+      .isNotEmpty()
+      .contains(type);
+  }
+
+  @ParameterizedTest
+  @EnumSource(value = ResourceTypeDictionary.class,
+    mode = EnumSource.Mode.EXCLUDE,
+    names = {"CONCEPT", "PERSON", "FAMILY", "JURISDICTION", "ORGANIZATION", "MEETING", "FORM", "TOPIC", "PLACE", "HUB",
+      "TEMPORAL"})
+  void notContainsAuthorityTypes(ResourceTypeDictionary type) {
+    //when
+    var expectedAuthorityTypes = ResourceKind.AUTHORITY;
+
+    //then
+    assertThat(expectedAuthorityTypes)
+      .isNotEmpty()
+      .doesNotContain(type);
+  }
+}
